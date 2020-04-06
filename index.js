2e53bdb37faa4c42a4dcd6753932e0ba5c2dd239
cosmologist10 <sumanshweta44@gmail.com>
2020-04-06 17:03:51 +0530

    pub-sub bind and connect interchanged

--- index.js
+++ index.js
@@ -1,29 +1,13 @@
-// const Telegraf = require('telegraf')
-//
-// // BOT TOKEN : 1060336780:AAHTn8MWDA4bHK97D4nEJbGPrhiri8ACFpU
-//
-// const bot = new Telegraf('1060336780:AAHTn8MWDA4bHK97D4nEJbGPrhiri8ACFpU')
-// bot.start((ctx) => ctx.reply('Hello'))
-// bot.help((ctx) => ctx.reply('Help message'))
-// bot.on('message', (ctx) => ctx.telegram.sendCopy(ctx.chat.id, ctx.message, Extra.markup(keyboard)))
-// bot.launch()
-
-
 const Telegraf = require('telegraf');
 const sqlite3 = require('sqlite3').verbose();
 
-// BOT TOKEN : 1060336780:AAHTn8MWDA4bHK97D4nEJbGPrhiri8ACFpU
-
 const bot = new Telegraf('1060336780:AAHTn8MWDA4bHK97D4nEJbGPrhiri8ACFpU');
-// bot.start((ctx) => ctx.reply('Hello'))
-// bot.help((ctx) => ctx.reply('Help message'))
-// bot.on('message', (ctx) => ctx.telegram.sendCopy(ctx.chat.id, ctx.message, Extra.markup(keyboard)))
+const db = new sqlite3.Database('./moonshot.db'); //Database path
+db.run("CREATE TABLE IF NOT EXISTS telegram (id TEXT NOT NULL PRIMARY KEY)");
+
 
 
 bot.command('/register', (ctx) => {
-
-    const db = new sqlite3.Database('./moonshot.db'); //Database path
-    db.run("CREATE TABLE IF NOT EXISTS telegram (id TEXT NOT NULL PRIMARY KEY)");
 
     let groupId = null;
 
@@ -37,7 +21,6 @@ bot.command('/register', (ctx) => {
                 groupId = row.id;
                 console.log(row.id);
             });
-            db.close();
             return bot.telegram.sendMessage(groupId, 'You have already been registered')
         } else {
             db.run(`insert into telegram(id) VALUES(?)`, [ctx.chat.id.toString()], function(err) {
@@ -45,7 +28,6 @@ bot.command('/register', (ctx) => {
                     throw err.message;
                 }
             });
-            db.close();
             return bot.telegram.sendMessage(ctx.chat.id, 'Registered')
         }
     });
@@ -53,7 +35,6 @@ bot.command('/register', (ctx) => {
 
 
 bot.command('/deregister', (ctx) => {
-    const db = new sqlite3.Database('./moonshot.db'); //Database path
     db.run('delete from telegram where id = ?', [ctx.chat.id.toString()], (err) => {
         if (err) {
             throw err.message;
@@ -64,19 +45,17 @@ bot.command('/deregister', (ctx) => {
 
 const run = function run() {
     const zmq = require("zeromq"),
-        sock = zmq.socket("sub");
+        sock = zmq.socket("pull");
 
-    sock.connect("tcp://192.168.100.2:1234");
-    sock.subscribe("experiment");
+    sock.bindSync("tcp://192.168.100.21:1234");
+    // sock.subscribe("experiment");
     console.log("Subscriber connected to port 1234");
 
-    sock.on("message", function (topic, message) {
+    sock.on("message", function (message) {
         console.log(
-            topic.toString(),
+            message,
             message.toString()
         );
-        const db = new sqlite3.Database('./moonshot.db'); //Database path
-        // let groupId = null;
         db.all('select id from telegram', [], (err, rows) => {
             if (err) {
                 throw err;
